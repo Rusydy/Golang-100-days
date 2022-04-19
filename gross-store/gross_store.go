63b5f16c6ab1603ddcf848e99aea30875912dc63
Rusydy <rusydymuhiddin@gmail.com>
2022-04-20 00:24:26 +0700

    consept: Maps

---
+++ gross-store/gross_store.go
@@ -0,0 +1,62 @@
+package gross
+
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
+
+}
+
+// NewBill creates a new bill.
+func NewBill() map[string]int {
+	return make(map[string]int)
+}
+
+// AddItem adds an item to customer bill.
+func AddItem(bill, units map[string]int, item, unit string) bool {
+	value, ok := units[unit]
+	if !ok {
+		return false
+	} else {
+		bill[item] += value
+		return true
+	}
+}
+
+// RemoveItem removes an item from customer bill.
+func RemoveItem(bill, units map[string]int, item, unit string) bool {
+
+	unitValue, unitExist := units[unit]
+	billValue, billExist := bill[item]
+
+	switch unitExist && billExist {
+	case false:
+		return false
+	default:
+		remaining := billValue - unitValue
+
+		if remaining == 0 {
+			delete(bill, item)
+			return true
+		} else if billValue < unitValue {
+			return false
+		} else {
+			bill[item] -= unitValue
+			return true
+		}
+	}
+
+}
+
+// GetItem returns the quantity of an item that the customer has in his/her bill.
+func GetItem(bill map[string]int, item string) (int, bool) {
+	value, ok := bill[item]
+	return value, ok
+}
